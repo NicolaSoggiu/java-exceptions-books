25cf583c77a1f59ff46886fe9b0a0d34fc317d95
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-24 14:53:39 +0200

    add array for the books

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -6,17 +6,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the book's title: ");
-        String title = scan.nextLine();
-        System.out.print("Enter the book's author: ");
-        String author = scan.nextLine();
-        System.out.print("Enter the book's publisher: ");
-        String publisher = scan.nextLine();
-        System.out.print("Enter the number of pages: ");
-        int pageNumber = scan.nextInt();
+        Book[] catalogue = new Book[100];
+        int count = 0;
 
-        Book book1 = new Book(title, author, publisher, pageNumber);
-        System.out.println(book1);
+        while (true) {
+            System.out.print("Enter the book's title: ");
+            String title = scan.nextLine();
+            System.out.print("Enter the book's author: ");
+            String author = scan.nextLine();
+            System.out.print("Enter the book's publisher: ");
+            String publisher = scan.nextLine();
+            System.out.print("Enter the number of pages: ");
+            int pageNumber = scan.nextInt();
+            scan.nextLine();
+
+            Book book1 = new Book(title, author, publisher, pageNumber);
+            System.out.println(book1);
+            catalogue[count] = book1;
+            count++;
+
+            System.out.print("Do you want to enter another book? (yes/no): ");
+            String response = scan.nextLine();
+            if (response.equalsIgnoreCase("no")) {
+                break;
+            }
+        }
+
+        System.out.println("Our Catalogue: ");
+        for (int i = 0; i < count; i++) {
+            System.out.println(catalogue[i].getTitle());
+        }
 
         scan.close();
     }
