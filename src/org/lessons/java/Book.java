76c70bec61d5ea4115b0c7e089a708008c2ebab4
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-24 14:28:12 +0200

    stamp one book to test the book class

--- src/org/lessons/java/Book.java
+++ src/org/lessons/java/Book.java
@@ -1,2 +1,55 @@
-package org.lessons.java;public class Book {
+package org.lessons.java;
+
+public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+    private int pageNumber;
+
+    public Book(String title, String author, String publisher, int pageNumber) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.pageNumber = pageNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + " " +
+                "\nAuthor: " + author + " " +
+                "\nPublisher by: " + publisher + " " +
+                "\nPage number: " + pageNumber;
+    }
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,2 +1,23 @@
-package org.lessons.java;public class Main {
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the book's title: ");
+        String title = scan.nextLine();
+        System.out.print("Enter the book's author: ");
+        String author = scan.nextLine();
+        System.out.print("Enter the book's publisher: ");
+        String publisher = scan.nextLine();
+        System.out.print("Enter the number of pages: ");
+        int pageNumber = scan.nextInt();
+
+        Book book1 = new Book(title, author, publisher, pageNumber);
+        System.out.println(book1);
+
+        scan.close();
+    }
 }
